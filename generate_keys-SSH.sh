00a0901889a92a3bb2522677bf8d0ca88b942eb2
Luiz Marroni <104742337+rownaas@users.noreply.github.com>
2024-06-21 13:49:05 -0300

    Update generate_keys-SSH.sh

--- generate_keys-SSH.sh
+++ generate_keys-SSH.sh
@@ -10,29 +10,32 @@ read -p "Digite o email do GitHub: " EMAIL
 read -s -p "Digite uma senha para a chave SSH (ou deixe em branco para sem senha): " PASSPHRASE
 echo
 
+# Define o caminho completo do diretório .ssh
+SSH_DIR="$HOME/.ssh"
+
 # Define o nome do arquivo da chave SSH
-KEY_NAME="id_rsa_github_${USERNAME}"
+KEY_NAME="$SSH_DIR/id_rsa_github_${USERNAME}"
 
 # Cria o diretório .ssh se não existir
-mkdir -p "$HOME/.ssh"
+mkdir -p "$SSH_DIR"
 
 # Verifica se a chave SSH já existe
-if [ -f "$HOME/.ssh/$KEY_NAME" ]; then
-    echo "A chave SSH $HOME/.ssh/$KEY_NAME já existe."
+if [ -f "$KEY_NAME" ]; then
+    echo "A chave SSH $KEY_NAME já existe."
     exit 1
 fi
 
 # Gera a chave SSH
-ssh-keygen -t rsa -b 4096 -C "$EMAIL" -f "$HOME/.ssh/$KEY_NAME" -N "$PASSPHRASE"
+ssh-keygen -t rsa -b 4096 -C "$EMAIL" -f "$KEY_NAME" -N "$PASSPHRASE"
 
 # Inicia o agente SSH
 eval "$(ssh-agent -s)"
 
 # Adiciona a chave SSH ao agente
-ssh-add "$HOME/.ssh/$KEY_NAME"
+ssh-add "$KEY_NAME"
 
 # Exibe a chave pública
 echo "A chave pública gerada é:"
-cat "$HOME/.ssh/$KEY_NAME.pub"
+cat "$KEY_NAME.pub"
 
 echo "Copie a chave acima e adicione-a ao seu GitHub, acessando https://github.com/settings/keys"
